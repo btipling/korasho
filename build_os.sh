45a1ada6ea72d9842f54e569b969bccb99ff55b0
Bjorn Tipling <bjorn@ambientchill.com>
2015-12-27 16:27:46 -0800

    OS X specific build file.

--- build_os.sh
+++ build_os.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# To compile on OS X brew install openssl and then:
 OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include \
   DEP_OPENSSL_INCLUDE=/usr/local/opt/openssl/include \
   OPENSSL_LIB_DIR=/usr/local/opt/openssl/include \
