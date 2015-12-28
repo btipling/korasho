9a21b07d354d164356b75a815a7e6eb0cf1627ad
Bjorn Tipling <bjorn@ambientchill.com>
2015-12-27 19:10:20 -0800

    Progress on reading from config.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
+extern crate toml;
+
+
+use std::fs::File;
+use std::env;
+use std::env::Args;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::str;
 
+struct Config<'a> {
+    host: & 'a str,
+    port: u16,
+}
+
+fn read_file_name(args: &mut Args) -> String {
+    let filename = if args.len() > 1 {
+        return args.nth(1).unwrap();
+    } else {
+        panic!("Need a config file path!");
+    };
+}
+
 fn main() {
     println!("Starting");
 
+    let filename = read_file_name(&mut env::args());
+    println!("Using config in {filename}", filename=filename);
+    //let config = readConfig(filename);
+    //println!("Using config with values: {host} {port}", host=config.host, port=config.port);
     let host = "chat.freenode.net";
     let port = "6667";
     let address = format!("{host}:{port}", host=host, port=port).to_string();
