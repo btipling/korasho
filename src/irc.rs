62e3818457383c07d152c5040c46ee10a12a2175
Bjorn Tipling <bjorn@ambientchill.com>
2016-01-09 13:37:08 -0800

    Progress on parsing server message.

--- src/irc.rs
+++ src/irc.rs
@@ -1,4 +1,5 @@
 use std::str;
+use std::slice::Split;
 
 #[derive(Default)]
 struct ConnectionState {
@@ -12,6 +13,25 @@ pub struct IRC {
     connection: ::connection::Connection,
     config: ::config::Config,
     connectionState: ConnectionState,
+}
+
+enum IRCMessageType {
+    NOTICE(String),
+}
+
+struct IRCServerMessage {
+    server: String,
+    message: IRCMessageType,
+    time: u64,
+}
+
+struct IRCCommMessage {
+    time: u64,
+}
+
+enum IRCMessage {
+    IRCServerMessage(IRCServerMessage),
+    IRCCommMessage(IRCCommMessage),
 }
 
 impl IRC {
@@ -44,7 +64,101 @@ impl IRC {
 
     fn process_line(&mut self, line: String) {
         println!("Processing line: {line}", line=line);
+        self.parse_line(line);
+        if !self.connectionState.identified {
+        }
     }
+
+    fn parse_server_line(&mut self, mut line_bytes: &[u8]) -> Option<IRCMessage> {
+        let mut new_line = line_bytes.clone();
+        let (_, line_bytes) = match new_line.split_first() {
+            Some(b) => b,
+            None => return None,
+        };
+        println!("lol: {:?}", String::from_utf8_lossy(line_bytes));
+        let mut line_iter = line_bytes.splitn(2, |x| *x == b':');
+        let meta_parts = match line_iter.next() {
+            Some(p) => p,
+            None => return None,
+        };
+        let message = match line_iter.next() {
+            Some(m) => String::from_utf8_lossy(m),
+            None => return None,
+        };
+        let mut meta_iter = meta_parts.splitn(2, |x| *x == b' ');
+        let server = match meta_iter.next() {
+            Some(m) => String::from_utf8_lossy(m),
+            None => return None,
+        };
+        let server_message_type = match meta_iter.next() {
+            Some(m) => String::from_utf8_lossy(m),
+            None => return None,
+        };
+        println!("\n\nServer: '{server}'\n message type: '{message_type}'\n message: {message}\n",
+                 server=server, message_type=server_message_type, message=message);
+        return None;
+    }
+
+    fn parse_comm_line(&mut self, mut line_bytes: &[u8]) -> Option<IRCMessage> {
+        return None;
+    }
+
+    fn parse_line(&mut self, mut line: String) -> Option<IRCMessage> {
+        let line_bytes: &[u8] = line.as_ref();
+        let len = line_bytes.len();
+        if len < 3 {
+            return None;
+        }
+        let line_bytes = &line_bytes[0..len-2];
+        if line_bytes[0] == b':' {
+            return self.parse_server_line(line_bytes);
+        }
+        return self.parse_comm_line(line_bytes);
+        /*
+        line = rest.to_string();
+        let i = match line.find(' ') {
+            Some(i) => i,
+            _ => return None,
+        };
+        new_line = line.clone();
+        let (server_str, rest) = new_line.split_at(i);
+        let server = server_str.to_string();
+        line = rest.to_string();
+        let i = match line.find(' ') {
+            Some(i) => i,
+            _ => return None,
+        };
+        new_line = line.clone();
+        let (message_type_str, rest) = new_line.split_at(i);
+        let message_type = message_type_str.clone();
+        line = rest.to_string();
+        let i = match line.find(':') {
+            Some(i) => i,
+            _ => return None,
+        };
+        new_line = line.clone();
+        let (_, message_str) = new_line.split_at(i);
+        let message = match self.make_message(&message_type, &message_str) {
+            Some(m) => m,
+            _ => return None,
+        };
+        Some(IRCMessage {
+            server: server,
+            message: message,
+            time: 0,
+        })
+        */
+
+    }
+
+    fn make_message(&mut self, message_type: &str, message: &str) -> Option<IRCMessageType> {
+        let stored_message = message.to_string();
+        match message_type {
+            "notice" => Some(IRCMessageType::NOTICE(stored_message)),
+            _ => None,
+        }
+    }
+
 
 }
 
